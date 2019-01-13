d64edac9488a84f766202642697d1704dd18d30b
smitus <46427067+smitus@users.noreply.github.com>
2019-01-13 21:28:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const Discord = require("discord.js");
 const robot = new Discord.Client();
 
-robot.login("NTE4NTY4MTU3NzExOTU4MDE3.DxwtWA.O6PVOQkqFqtaVGN0FKOjGWvuZ4I");
+robot.login("NTE4NTY4MTU3NzExOTU4MDE3.DxzQ8g.l8u4ZLFuHJZiZuIFOWTXbgihkg4");
 
 robot.on('message', (message) => {
   if (message.content == "qd") {
     message.delete()
-    message.reply("стартует поиск игры!!! (НОРМАЛ РЕЖИМ ВЫБОРА!) Ждите своей очереди!:loudspeaker::loudspeaker::loudspeaker:");
+    message.channel.send(":zap: :zap: :zap:  стак " + message.author + " стартует поиск игры!!! :zap:(НОРМАЛ РЕЖИМ ВЫБОРА!):zap: Ждите своей очереди!:loudspeaker::loudspeaker::loudspeaker:");
    
   }
   if (message.content == "qb") {
     message.delete()
-    message.reply("стартует поиск игры!!! (НОРМАЛ ВЫБОР В СЛЕПУЮ!) Ждите своей очереди!:loudspeaker::loudspeaker::loudspeaker:");
+    message.channel.send(":zap: :zap: :zap:  стак " + message.author + " стартует поиск игры!!! :zap:(НОРМАЛ ВЫБОР В СЛЕПУЮ!):zap: Ждите своей очереди!:loudspeaker::loudspeaker::loudspeaker:");
   
   }
   if (message.content == "qf") {
     message.delete()
-    message.reply("стартует поиск игры!!! (ФЛЕКС!) Ждите своей очереди!:loudspeaker::loudspeaker::loudspeaker:");
+    message.channel.send(":zap: :zap: :zap:  стак " + message.author + " стартует поиск игры!!! :zap:(ФЛЕКС!):zap: Ждите своей очереди!:loudspeaker::loudspeaker::loudspeaker:");
  
   }
   if (message.content == "p") {
     message.delete()
-    message.reply("закончил поиск матча! Можно стартовать игру остальным соклубникам.:checkered_flag::checkered_flag::checkered_flag:");
+    message.channel.send(":checkered_flag::checkered_flag: :checkered_flag: стак " + message.author + " закончил поиск матча! Можно стартовать игру остальным соклубникам.:checkered_flag::checkered_flag::checkered_flag:");
   }
 });
