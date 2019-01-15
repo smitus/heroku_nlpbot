4b199c62419f1a3ec8351c26c54f11c366fdc38d
smitus <46427067+smitus@users.noreply.github.com>
2019-01-16 01:41:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
   const Discord = require("discord.js");
   const robot = new Discord.Client();
 
-  robot.login("NTE4NTY4MTU3NzExOTU4MDE3.DxzQ8g.l8u4ZLFuHJZiZuIFOWTXbgihkg4");
+  robot.login("NTE4NTY4MTU3NzExOTU4MDE3.DxzQ8g.l8bgihkg4");
 
   robot.on('message', (message) => {
     if (message.content == "qd") {
